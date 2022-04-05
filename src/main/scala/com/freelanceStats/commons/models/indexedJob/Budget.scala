c2e238f82d15a3a32ce5a7efa5394882dd8e83c0
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-04-05 21:41:18 +0200

    Made budget minimum and maximum optional

--- src/main/scala/com/freelanceStats/commons/models/indexedJob/Budget.scala
+++ src/main/scala/com/freelanceStats/commons/models/indexedJob/Budget.scala
@@ -1,9 +1,9 @@
 package com.freelanceStats.commons.models.indexedJob
 
 case class Budget(
-    minimumUsd: Double,
-    maximumUsd: Double,
-    minimum: Double,
-    maximum: Double,
+    minimumUsd: Option[Double],
+    maximumUsd: Option[Double],
+    minimum: Option[Double],
+    maximum: Option[Double],
     currency: Option[Currency]
 )
