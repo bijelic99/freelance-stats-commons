973ba7dd7535a4c7d0b8566fc23238636550a521
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-08-21 11:44:06 +0200

    Added commit messages

--- project/CommonConfig.scala
+++ project/CommonConfig.scala
@@ -1,10 +1,14 @@
 import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
-import sbt.Keys.{organization, scalaVersion}
+import sbt.Keys.{name, organization, scalaVersion, version}
+import sbtrelease.ReleasePlugin.autoImport.{releaseCommitMessage, releaseNextCommitMessage, releaseTagComment}
 
 object CommonConfig {
   lazy val get = Seq(
     organization := "com.freelance-stats",
     scalaVersion := "2.13.6",
-    scalafmtOnCompile := true
+    scalafmtOnCompile := true,
+    releaseTagComment := s"Releasing ${name.value} ${version.value}",
+    releaseCommitMessage := s"Setting version of ${name.value} to ${version.value}",
+    releaseNextCommitMessage := s"Setting version of ${name.value} to ${version.value}",
   )
 }
